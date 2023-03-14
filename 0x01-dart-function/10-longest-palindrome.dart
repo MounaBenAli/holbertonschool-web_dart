41285040d6adea9f47ba32078354f78b6c6b83a0
MounaBenAli <benalimouna1986@gmail.com>
2023-03-14 12:02:39 +0100

    10. Longest Palindrome

---
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -0,0 +1,24 @@
+import '9-palindrome.dart';
+
+String longestPalindrome(String s) {
+  String longestPalindrome = '';
+
+if (s.length < 3) {
+    return 'none';
+  }
+
+for (int i = 0; i < s.length; i++)
+{
+  //for (int j = i + 1; j <= s.length; j++)
+  //{
+    String substr = s.substring(i);
+     print('substring: $substr');
+      if(isPalindrome(substr) && substr.length > longestPalindrome.length)
+    {
+      longestPalindrome= substr; 
+    }
+  //}
+}
+  if (longestPalindrome.isEmpty) return 'none';
+return longestPalindrome;
+}
