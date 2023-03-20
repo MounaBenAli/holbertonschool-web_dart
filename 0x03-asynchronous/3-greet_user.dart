00c4e8020ad6fcc1e759f4259689bdc50d3f8abd
MounaBenAli <benalimouna1986@gmail.com>
2023-03-20 12:03:25 +0100

    3. Greet User

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+Future<String> fetchUserData() => Future.delayed(
+      const Duration(seconds: 2),
+      () =>
+          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
+    );
+
+Future<bool> checkCredentials() =>
+    Future.delayed(const Duration(seconds: 2), () => true);
+
+Future<String> greetUser() async{ 
+    try {
+      var user = await fetchUserData();
+      var fetch = jsonDecode(user);
+      var username = fetch['username'];
+
+      return 'Hello $username';
+    } catch(error) {
+      return ('Error caught: $error');
+    }
+}
+
+Future<String> loginUser() async{
+ try {
+    var check = await checkCredentials();
+
+    if (check == true) {
+      print('There is a user: true');
+      return await greetUser();
+    } else {
+      print ('There is a user: false ');
+      return ('Wrong credentials');
+    }     
+  } catch(error) {
+      return ('Error caught: $error');
+  }
+}
