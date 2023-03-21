28dc310d18ec62b41a24b2acbb317d9af1a0b158
MounaBenAli <benalimouna1986@gmail.com>
2023-03-21 19:25:31 +0100

    1. Get User ID

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,16 +1,12 @@
 import 'dart:convert';
+import '1-util.dart';
 
-Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
 
 getUserId() async {
   var user = await fetchUserData();
 
   Map<String, dynamic> jsonMap = json.decode(user);
-  
+
   return jsonMap['id'];
 
 }
