90b6b55ac4fc009a516c7a8e77fd449e4d72e99a
MounaBenAli <benalimouna1986@gmail.com>
2023-03-21 20:31:20 +0100

    5. Rick and Morty Characters

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,14 +1,13 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 Future<void> printRmCharacters() async {
   try {
-    String apiUrl = 'https://rickandmortyapi.com/api/character';
-    http.Response response = await http.get(Uri.parse(apiUrl));
+    final apiUrl = 'https://rickandmortyapi.com/api/character';
+    var  response = await http.get(Uri.parse(apiUrl));
     if (response.statusCode == 200) {
-      Map<String, dynamic> jsonResponse = json.decode(response.body);
-      List<dynamic> characters = jsonResponse['results'];
+      var jsonResponse = json.decode(response.body);
+      var characters = jsonResponse['results'];
       characters.forEach((character) {
         print(character['name']);
       });
